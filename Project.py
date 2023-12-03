52111802596bbeab12204f63f6b63b0c58104c9c
Josh Gillum <jgillum@students.kennesaw.edu>
2023-12-02 23:19:41 -0500

    starting the basis

---
+++ Project.py
@@ -0,0 +1,10 @@
+print("Welcome to your Budget Helper!")
+
+monthStatus = input("Enter 'A' if you would like to edit this month's budget or 'B' to move onto next month")
+if monthStatus == "A":
+    currentMonth == input("What is the current month")
+    fillerFunction
+else:
+    otherFillerFunction
+
+def fillerfunction():
